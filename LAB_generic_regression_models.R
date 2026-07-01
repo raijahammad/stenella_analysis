47e36ddfb35a16a63e5179d54ae20d3570197eaf
raija hammad <raijahammad@MacBook-Pro-7.local>
2026-07-01 13:31:11 -0700

    generic workflow for estimating LAB

--- LAB_generic_regression_models.R
+++ LAB_generic_regression_models.R
@@ -1,3 +1,7 @@
+# install necessary packages
+install.packages("brms")
+library(brms)
+
 # hard coding so neonates are classified as individuals under 125cm
 NEONATE_MAX_LENGTH <- 125
 
@@ -26,15 +30,29 @@ table(birth_data$Born)
 summary(birth_data$Length)
 
 
-# this is the model itself
+# frequentist logistic regression
 birth_model <- glm(
   Born ~ Length,
   family = binomial,
   data = birth_data
 )
-
-# checking things out
 summary(birth_model)
+
+# Bayesian logistic regression
+bayes_birth_model <- brm(
+  Born ~ Length,
+  data = birth_data,
+  family = bernoulli(link = "logit"),
+  prior = c(
+    prior(normal(0, 5), class = "Intercept"),
+    prior(normal(0, 2), class = "b")
+  ),
+  chains = 4,
+  iter = 4000,
+  warmup = 1000,
+  seed = 123
+)
+summary(bayes_birth_model)
 
 # now estimate length at birth!
 length_at_birth <- -coef(birth_model)[1] / coef(birth_model)[2]
